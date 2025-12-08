8f036234b1803983e99548a30e6d4a3193407078
xellossrilla <xellossrilla@gmail.com>
2025-12-08 17:42:16 +0900

    2025-12-08 수업 jsp와의 연계

---
+++ 2025-12-08 수업.sql
@@ -0,0 +1,13 @@
+SELECT * FROM bbs_t;
+SELECT * FROM comment_t;
+-- 테이블 내의 모든 데이터를 복사하여 다시 넣기
+insert into bbs_t(b_idx, writer, title, content, pw, hit, ip, filename, reg_date)
+select bbs_seq.nextval, writer, title, content, pw, hit, ip, filename, sysdate
+from bbs_t;
+-- 페이지 번호에 맞는 데이터를 기준 갯수만큼 select 하는 쿼리
+SELECT b_idx, writer, title, hit, reg_date FROM
+				(SELECT ROWNUM rn, a.* FROM 
+					(SELECT b_idx, writer, title, hit, reg_date FROM bbs_t ORDER BY b_idx DESC) a 
+				WHERE ROWNUM <= pageNum * amount)
+			WHERE rn > (pageNum-1) * amount;
+commit;
